c6404decd3cd7c75dd02c1a331912893d874ccde
Bagus Nararya Nanda Raditya <87893990+nararyacodetechy@users.noreply.github.com>
2024-04-04 17:26:43 +0800

    Add Routes Validator

---
+++ frontend/src/pages/dashboard/DetailValidatorPage.jsx
@@ -0,0 +1,11 @@
+const DetailValidatorPage = () => {
+  return (
+    <div>
+      <h1 className="mb-10">Detail Validator Page</h1>
+      <p>Disini isi Informasi Pribadi Validator</p>
+      <p>Disini isi Progress legit check validator</p>
+    </div>
+  );
+};
+
+export default DetailValidatorPage;
