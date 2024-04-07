d22ffc98bef78bca2affe8dbd0614b75caee8cb8
Bagus Nararya Nanda Raditya <87893990+nararyacodetechy@users.noreply.github.com>
2024-04-07 23:01:48 +0800

    Add Not Found Pafe

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { AuthLayout } from './layouts/AuthLayout';
 import { DashboardLayout } from './layouts/DashboardLayout';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   return (
     <Routes>
       <Route path="/admin-role/*" element={<DashboardLayout />} />
       <Route path="/validator-role/*" element={<DashboardLayout />} />
+      <Route path="*" element={<NotFoundPage />} />
       <Route path="/auth/*" element={<AuthLayout />} />
-      <Route path="*" element={<Navigate to="/auth/sign-in" replace />} />
+      <Route path="/" element={<Navigate to="/auth/sign-in" replace />} />
     </Routes>
   );
 }
---
+++ frontend/src/pages/NotFoundPage.jsx
@@ -0,0 +1,13 @@
+const NotFoundPage = () => {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <div className="text-center">
+          <h1 className="text-2xl font-bold">404 Not Found</h1>
+          <p className="mt-4">Maaf, halaman yang Anda cari tidak ditemukan.</p>
+        </div>
+      </div>
+    );
+  };
+  
+  export default NotFoundPage;
+  
