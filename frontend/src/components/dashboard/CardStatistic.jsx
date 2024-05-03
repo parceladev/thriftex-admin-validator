789b8a4f8da8b871a48600d576f0d2aebce9abcc
Gerry-Latris <whateveritakes10@gmail.com>
2024-05-04 02:12:21 +0800

    fix styling in StatisticCard

--- frontend/src/components/dashboard/CardStatistic.jsx
+++ frontend/src/components/dashboard/CardStatistic.jsx
@@ -5,13 +5,13 @@ const CardStatistic = (props) => {
   const { title, icon, alt, content } = props;
   return (
     <div className="p-8 flex justify-between items-center border-[#13A9D9] border-2 rounded-md">
-      <div className="flex flex-col gap-3">
+      <div className="flex flex-col gap-2 items-center text-secondary font-sans">
         <FontAwesomeIcon icon={icon} alt={alt} className='w-9 h-9 text-secondary' />
-        <h2 className="text-[18px]">{content}</h2>
+        <h2 className="text-[25px] font-semibold">{content}</h2>
       </div>
-      <div className="flex self-end">
-        <h2 className="text-[18px]">{title}</h2>
-      </div>
+      <div className="flex self-end text-secondary font-medium">
+        <h2 className="text-[18px] ">{title}</h2>
+      </div>  
     </div>
   );
 };
